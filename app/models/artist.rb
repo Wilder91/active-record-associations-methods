a044e85e0747fc27194df61403e1f35b2742dadc
Mike Wilder <mikewilder01@gmail.com>
2021-01-18 12:58:08 -0500

    commit tree 5dff29488478559652ce501de761b339371d8810

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,18 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    #binding.pry
+    self.songs.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    #binding.pry
+    self.songs.size
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.genres.size
   end
 end
